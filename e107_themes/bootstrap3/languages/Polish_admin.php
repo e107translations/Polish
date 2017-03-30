e5540eadd81e72d3d507dbfa530470f556356267
Jacek <jacek_cich@poczta.fm>
2017-03-30 22:22:56 +0200

    Add files via upload

--- e107_themes/bootstrap3/languages/Polish_admin.php
+++ e107_themes/bootstrap3/languages/Polish_admin.php
@@ -1,3 +1,26 @@
 <?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Polish Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/03/28 20:43:59
+|
+|        $Author: Jacek Cichowski $
++---------------------------------------------------------------+
+*/
 
-define("EXAMPLE","Generated Empty Language File");
+define("LAN_THEMEPREF_00", "branding");
+define("LAN_THEMEPREF_01", "Wyrównanie paska nawigacji:");
+define("LAN_THEMEPREF_02", "Rejestracja / Logowanie:");
+define("LAN_THEMEPREF_03", "Style Bootswatch:");
+define("LAN_THEMEPREF_04", "Nazwa strony");
+define("LAN_THEMEPREF_05", " Logo");
+define("LAN_THEMEPREF_06", " Logo &amp; Nazwa strony");
+define("LAN_THEMEPREF_07", "lewo");
+define("LAN_THEMEPREF_08", "prawo");
+define("LAN_THEMEPREF_09", "góra");
+define("LAN_THEMEPREF_10", "dół");
+
+
+?>
