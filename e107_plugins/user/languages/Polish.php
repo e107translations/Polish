40ab2dfff34b46951e4117e9d34cce3d2920f787
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:53:18 +0200

    Add files via upload

--- e107_plugins/user/languages/Polish.php
+++ e107_plugins/user/languages/Polish.php
@@ -1,18 +1,30 @@
 <?php
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/03/22                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
 
-define("UTHEME_MENU_L1", "Zestaw");
-define("UTHEME_MENU_L2", "Wybierz język");
+define("UTHEME_MENU_L1", "Ustawienia");
+define("UTHEME_MENU_L2", "Wybór języka");
 define("UTHEME_MENU_L3", "tabele");
-define("LAN_UMENU_THEME_1", "Ustaw motyw");
-define("LAN_UMENU_THEME_2", "Wybierz motyw");
-define("LAN_UMENU_THEME_3", "Użytkownicy:");
-define("LAN_UMENU_THEME_4", "Po tych tematów, które użytkownicy mogą wybrać");
-define("LAN_UMENU_THEME_5", "Aktualizacja");
+
+define("LAN_UMENU_THEME_1", "Zmień motyw");
+define("LAN_UMENU_THEME_2", "Wybór motywu");
+define("LAN_UMENU_THEME_3", "użytkownicy:");
+define("LAN_UMENU_THEME_4", "Wybierz motywy, które będą dostępne dla użytkowników");
+define("LAN_UMENU_THEME_5", "Aktualizuj");
 define("LAN_UMENU_THEME_6", "Motywy dostępne dla użytkowników");
-define("LAN_UMENU_THEME_7", "Klasy, które można wybrać tematy");
-define("LAN_UMENU_THEME_8", "Dozwolonych tematy:");
-define("LAN_UMENU_THEME_9", "Klasy, które można wybrać tematy:");
+define("LAN_UMENU_THEME_7", "Grupa użytkowników uprawniona do zmiany motywu");
+define("LAN_UMENU_THEME_8", "Dozwolone tematy: ");
+define("LAN_UMENU_THEME_9", "Klasy, które mogą wybrać tematy: ");
+
+?>
---
+++ e107_plugins/user/languages/Polish_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Polish Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/03/27 21:54:42
+|
+|        $Author: Jacek Cichowski $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_USER_NAME", "Użytkownik");
+define("LAN_PLUGIN_USER_DESC", "Szablon użytkownika i menu językowe");
+
+
+?>
