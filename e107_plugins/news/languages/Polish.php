317b16cc1c76e0dcc7262c9290b372a12e0f6de0
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:46:18 +0200

    Add files via upload

--- e107_plugins/news/languages/Polish.php
+++ e107_plugins/news/languages/Polish.php
@@ -1,10 +1,20 @@
 <?php
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/03/22                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+define("TD_MENU_L1", "Pozostałe aktualności");
+define("TD_MENU_L2", "Pozostałe aktualności");
 
-define("TD_MENU_L1", "Inne aktualności");
-define("TD_MENU_L2", "Inne aktualności");
-define("LAN_NEWSCAT_MENU_TITLE", "Kategorie wiadomości");
-define("LAN_NEWSLATEST_MENU_TITLE", "Najnowsze wiadomości");
+define("LAN_NEWSCAT_MENU_TITLE", "Kategorie aktualności");
+define("LAN_NEWSLATEST_MENU_TITLE", "Najnowsze aktualności");
+?>
---
+++ e107_plugins/news/languages/Polish_admin.php
@@ -0,0 +1,27 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Polish Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/03/28 19:36:31
+|
+|        $Author: Jacek Cichowski $
++---------------------------------------------------------------+
+*/
+
+define("LAN_NEWS_ADMIN_00", "Ostatnie aktualności");
+define("LAN_NEWS_ADMIN_01", "Przyklejone aktualności");
+define("LAN_NEWS_ADMIN_02", "Asygnowane aktualności");
+define("LAN_NEWS_ADMIN_03", "Limit wiadomości do konkretnej kategorii");
+define("LAN_NEWS_ADMIN_04", "Przypisane elementy to szablony przypisane do 'News Grid Menu'");
+define("LAN_NEWS_ADMIN_05", "Ilość do wyświetlania");
+define("LAN_NEWS_ADMIN_06", "Ilość znaków w tytule.");
+define("LAN_NEWS_ADMIN_07", "Łączny limit znaków");
+define("LAN_NEWS_ADMIN_08", "Pokazywać link do archiwum");
+define("LAN_NEWS_ADMIN_09", "Limity");
+define("LAN_NEWS_ADMIN_10", "Liczba elementów funkcji");
+define("LAN_NEWS_ADMIN_11", "Elementy przypisane będą pokazywane w szablonie 'News Carousel'");
+
+
+?>
