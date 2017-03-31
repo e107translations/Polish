633632c92caeb4d6b80d2179f5b68bc0d7ed6bcf
Jacek <jacek_cich@poczta.fm>
2017-03-31 19:14:21 +0200

    Add files via upload

--- e107_plugins/_blank/languages/Polish/Polish_global.php
+++ e107_plugins/_blank/languages/Polish/Polish_global.php
@@ -1,9 +1,6 @@
 <?php
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("LAN_PLUGIN__BLANK_NAME", "Puste Plugin");
-define("LAN_PLUGIN__BLANK_DIZ", "Puste Plugin, aby pomóc Ci rozpocząć pracę w rozwoju wtyczki. Więcej szczegółów można dodawać tutaj.");
-define("LAN_PLUGIN__BLANK_LINK", "Puste Link");
+define("LAN_PLUGIN__BLANK_NAME", "Pusta wtyczka");
+define("LAN_PLUGIN__BLANK_DIZ",  "Pusta wtyczka umożliwia pisanie własnych wtyczek. Więcej szczegółów zostanie tu napisane.");
+define("LAN_PLUGIN__BLANK_LINK", "Pusty link");
+?>
