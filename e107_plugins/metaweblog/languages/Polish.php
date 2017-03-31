8a925a6ef98808b8715854854e4713a2e2bf4b23
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:45:16 +0200

    Add files via upload

--- e107_plugins/metaweblog/languages/Polish.php
+++ e107_plugins/metaweblog/languages/Polish.php
@@ -1,27 +1,44 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/03/30                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
 define("XMLRPC_ADMIN_001", "Menu główne");
-define("XMLRPC_CONFIG_001", "MetaWeblog:: Konfiguracja");
-define("XMLRPC_PREFS_001", "eXMLRPC - opcje");
-define("XMLRPC_PREFS_002", "eXMLRPC");
-define("XMLRPC_PREFS_003", "Typ renderowania News (0,1,2,3)");
-define("XMLRPC_PREFS_004", "Przesłać katalog (e_FILE /)");
-define("XMLRPC_PREFS_005", "Identyfikator bloga dla klienta");
-define("XMLRPC_PREFS_006", "Nazwa bloga dla klienta");
+// define("XMLRPC_ADMIN_002", "Configuration"); use common e107 LANS:  LAN_CONFIGURATION
+// define("XMLRPC_ADMIN_003", "Admin");
+// define("XMLRPC_ADMIN_004", "Options"); LAN_OPTIONS
+// define("XMLRPC_ADMIN_005", "Prefs"); LAN_PREFS
+
+define("XMLRPC_CONFIG_001", " Metaweblog :: Ustawienia");
+
+define("XMLRPC_PREFS_001", " eXMLRPC - Opcje");
+define("XMLRPC_PREFS_002", " eXMLRPC");
+define("XMLRPC_PREFS_003", "Styl wyświetlania aktualności (0,1,2,3)");
+define("XMLRPC_PREFS_004", "Katalog plików nadsyłanych (e_FILE/)");
+define("XMLRPC_PREFS_005", "ID klienta bloga");
+define("XMLRPC_PREFS_006", "Nazwa użytkownika bloga dla klienta");
+
 define("XMLRPC_HELP_001", "Instrukcje");
 define("XMLRPC_HELP_010", "Ogólne");
-define("XMLRPC_HELP_011", "Ten plugin, sam nic nie robi! To jest tylko dla ustawienia niektórych varibles XMLRPC. Wskaż klienta XMLRPC (np: Windows Live Writer) do <strong>'. SITEURL.' MetaWeblog.php</strong> i wypełnij żądane wartości. Ostrzeżenie! Ten plugin jest eksperymentalna!");
-define("XMLRPC_HELP_020", "Tytuł plugin");
-define("XMLRPC_HELP_021", "Bez względu na plugin nie wyjście");
-define("XMLRPC_HELP_030", "Typ renderowania Aktualności");
-define("XMLRPC_HELP_031", "Ważne! Domyślny typ renderowania Aktualności");
-define("XMLRPC_HELP_040", "Prześlij folder dla plików");
-define("XMLRPC_HELP_041", "Zawsze jest to folder wewnątrz e107_files!");
-define("XMLRPC_HELP_050", "Identyfikator bloga dla klienta");
-define("XMLRPC_HELP_051", "Identyfikator bloga dla klienta XMLRPC (dowolny ciąg, który ma)");
-define("XMLRPC_HELP_060", "Nazwa bloga dla klienta");
-define("XMLRPC_HELP_061", "Nazwa bloga dla klienta XMLRPC (dowolny ciąg, który ma)");
+define("XMLRPC_HELP_011", "Ten sam plugin nic nie robi! Służy jedynie ustawieniom dla zmiennych XMLRPC. Przekieruj swojego klienta XMLRPC (np: Windows Live Writer) na <strong>".SITEURL."metaweblog.php</strong> i wypełnij wymagane wartości. Ostrzeżenie! Wtyczka ta jest eksperymentalna!");
+define("XMLRPC_HELP_020", "Nazwa wtyczki");
+define("XMLRPC_HELP_021", "Brak wtyczki nie ma znaczenia");
+define("XMLRPC_HELP_030", "Styl wyświetlania aktualności");
+define("XMLRPC_HELP_031", "Ważne! Domyślny typ wyświetlania wiadomości");
+define("XMLRPC_HELP_040", "Dodaj folder dla plików");
+define("XMLRPC_HELP_041", "To jest zawsze wewnątrz folderu e107_files!");
+define("XMLRPC_HELP_050", "ID klienta bloga");
+define("XMLRPC_HELP_051", "ID klienta bloga XMLRPC (wpisz co chcesz)");
+define("XMLRPC_HELP_060", "Nazwa użytkownika bloga dla klienta");
+define("XMLRPC_HELP_061", "Nazwa użytkownika bloga dla klienta XMLRPC (wpisz co chcesz)");
+
+?>
