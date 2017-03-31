a7a558d0e31353b456185c785b64a9d9f1752ada
Jacek <jacek_cich@poczta.fm>
2017-03-31 19:16:17 +0200

    Add files via upload

--- e107_plugins/blogcalendar_menu/languages/Polish.php
+++ e107_plugins/blogcalendar_menu/languages/Polish.php
@@ -1,13 +1,26 @@
 <?php
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/02/20                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("BLOGCAL_L1", "Wiadomości przez miesiąc");
+define("BLOGCAL_L1", "Aktualności");
 define("BLOGCAL_L2", "Archiwum");
-define("BLOGCAL_1", "Aktualności");
-define("BLOGCAL_CONF1", "Miesięcy lub wiersza");
-define("BLOGCAL_CONF2", "CellPadding");
-define("BLOGCAL_CONF4", "BlogCal Menu konfiguracji");
-define("BLOGCAL_ARCHIV1", "Wybierz Archiwum");
+define("BLOGCAL_1", "Pozycja aktualności");
+
+define("BLOGCAL_CONF1", "Ilość wyświetlanych miesięcy w wierszu");
+define("BLOGCAL_CONF2", "Odstęp pomiędzy komórkami");
+//define("BLOGCAL_CONF3", "Update Menu Settings");
+define("BLOGCAL_CONF4", "Konfiguracja menu Kalendarz");
+//define("BLOGCAL_CONF5", "BlogCal menu configuration saved");
+define("BLOGCAL_ARCHIV1", "Wybierz archiwum");
+
+?>
