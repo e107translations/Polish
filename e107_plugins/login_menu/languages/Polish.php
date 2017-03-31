87fbb8deadd94f8aa91aa4a045729b3c595c3f92
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:44:19 +0200

    Add files via upload

--- e107_plugins/login_menu/languages/Polish.php
+++ e107_plugins/login_menu/languages/Polish.php
@@ -1,51 +1,76 @@
 <?php
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/03/22                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("LAN_LOGINMENU_1", "Nazwa użytkownika:");
-define("LAN_LOGINMENU_2", "Hasło:");
-define("LAN_LOGINMENU_3", "Zarejestruj się");
-define("LAN_LOGINMENU_4", "Zapomniałeś hasła?");
-define("LAN_LOGINMENU_5", "Witamy");
+define("LAN_LOGINMENU_1", "Login: ");
+define("LAN_LOGINMENU_2", "Hasło: ");
+define("LAN_LOGINMENU_3", "Rejestracja");
+define("LAN_LOGINMENU_4", "Zmiana hasła");
+define("LAN_LOGINMENU_5", "Witaj");
 define("LAN_LOGINMENU_6", "Zapamiętaj mnie");
-define("LAN_LOGINMENU_7", "Unikatowy identyfikator użytkownika nie ujmuje (możliwe uszkodzonego pliku cookie). Kliknij poniżej, aby zniszczyć cookie łącze Wyloguj.");
+define("LAN_LOGINMENU_7", "Unikalne ID użytkownika nie zostało rozpoznane (prawdopodobnie uszkodzony jest plik <o>cookie</i>). Proszę kliknąć Wyloguj się, aby usunąć plik cookie.");
+// define("LAN_LOGINMENU_8", "Logout");
 define("LAN_LOGINMENU_9", "Błąd logowania");
-define("LAN_LOGINMENU_10", "Konserwacji flaga jest wartość true - oznacza to, że normalne odwiedzający są przekierowywane do sitedown.php. Aby zresetować flagi przejdź do admin/konserwacji.");
-define("LAN_LOGINMENU_11", "Admin Area");
-define("LAN_LOGINMENU_13", "Profil użytkownika");
-define("LAN_LOGINMENU_14", "Nowość");
-define("LAN_LOGINMENU_15", "Aktualności");
-define("LAN_LOGINMENU_16", "ChatBox post");
-define("LAN_LOGINMENU_17", "ChatBox postów");
+define("LAN_LOGINMENU_10", "Strona jest w trybie konserwacji - to oznacza, że osoby odwiedzające Twoją stronę zostaną przekierowane do strony sitedown.php. Aby przywrócić normalne funkcjonowanie witryny przejdź do panelu administratora i zmień odpowiednie opcje w dziale <i>Konserwacja</i>.");
+define("LAN_LOGINMENU_11", "Panel admina");
+// define("LAN_LOGINMENU_12", "Settings");
+define("LAN_LOGINMENU_13", "Profil");
+define("LAN_LOGINMENU_14", "wiadomość");
+define("LAN_LOGINMENU_15", "wiadomości");
+define("LAN_LOGINMENU_16", "post na czacie");
+define("LAN_LOGINMENU_17", "postów na czacie");
 define("LAN_LOGINMENU_18", "komentarz");
-define("LAN_LOGINMENU_19", "Komentarze");
+define("LAN_LOGINMENU_19", "komentarzy");
 define("LAN_LOGINMENU_20", "post na forum");
-define("LAN_LOGINMENU_21", "Posty na forum");
-define("LAN_LOGINMENU_22", "Nowy użytkownik witryny");
-define("LAN_LOGINMENU_23", "nowych członków witryny");
-define("LAN_LOGINMENU_24", "Kliknij tutaj, aby zobaczyć listę nowych elementów");
-define("LAN_LOGINMENU_25", "Od Twojej ostatniej wizyty były");
-define("LAN_LOGINMENU_26", "nie");
-define("LAN_LOGINMENU_27", "i");
-define("LAN_LOGINMENU_31", "Pokaż nowe wiadomości");
-define("LAN_LOGINMENU_34", "Pokaż nowe posty komentarz");
-define("LAN_LOGINMENU_36", "Pokaż nowych członków");
-define("LAN_LOGINMENU_39", "Pozostawić Admin");
-define("LAN_LOGINMENU_40", "Wyślij ponownie E-mail aktywacyjny");
-define("LAN_LOGINMENU_41", "Ustawienia Menu logowania");
+define("LAN_LOGINMENU_21", "postów na forum");
+define("LAN_LOGINMENU_22", "nowy użytkownik");
+define("LAN_LOGINMENU_23", "nowych użytkowników");
+define("LAN_LOGINMENU_24", "Kliknij tutaj, aby zobaczyć listę nowych pozycji");
+define("LAN_LOGINMENU_25", "Od Twojej ostatniej wizyty na stronie pojawiło się");
+define("LAN_LOGINMENU_26", "brak");
+define("LAN_LOGINMENU_27", "oraz");
+//define("LAN_LOGINMENU_28", "Login");
+//define("LAN_LOGINMENU_29", "new article");
+//define("LAN_LOGINMENU_30", "new articles");
+
+// New config options
+define("LAN_LOGINMENU_31", "Pokaż nowe aktualności");
+//define("LAN_LOGINMENU_32", "Show New Article Posts");
+//define("LAN_LOGINMENU_33", "Show New Chatbox Posts");
+define("LAN_LOGINMENU_34", "Pokaż nowe komentarze");
+//define("LAN_LOGINMENU_35", "Show New Forum Posts");
+define("LAN_LOGINMENU_36", "Pokaż nowo zarejestrowanych użytkowników");
+
+
+define("LAN_LOGINMENU_39", "Opuść panel");
+define("LAN_LOGINMENU_40", "Ponów aktywację");
+define("LAN_LOGINMENU_41", "Ustawienia - Login Menu");
+
+//new v0.8
 define("LAN_LOGINMENU_37", "Pokaż");
-define("LAN_LOGINMENU_38", "Okno logowania - dodatkowe łącza");
-define("LAN_LOGINMENU_42", "Okno logowania - ostatnio dodane core");
-define("LAN_LOGINMENU_43", "Pozycji");
-define("LAN_LOGINMENU_44", "brakujące ogniwo tytuł");
-define("LAN_LOGINMENU_45", "Link (s)-");
-define("LAN_LOGINMENU_45a", "");
-define("LAN_LOGINMENU_45b", "plugin");
-define("LAN_LOGINMENU_46", "Niedawno używane elementy-");
-define("LAN_LOGINMENU_47", "Okno logowania - najnowszych dodatków plugin");
-define("LAN_LOGINMENU_48", "Menu konfiguracji");
-define("LAN_LOGINMENU_49", "Adres e-mail:");
-define("LAN_LOGINMENU_50", "Nazwa użytkownika lub adres E-mail:");
-define("LAN_LOGINMENU_51", "Rejestrowanie");
+define("LAN_LOGINMENU_38", "Login menu - Dodatkowe linki");
+
+define("LAN_LOGINMENU_42", "Login menu - Najnowsze dodatki rdzeniowe");
+define("LAN_LOGINMENU_43", "Pozycja");
+define("LAN_LOGINMENU_44", "brakujący tytuł linku");
+define("LAN_LOGINMENU_45", "link(i) -");
+define("LAN_LOGINMENU_45a", ""); //pre
+define("LAN_LOGINMENU_45b", "wtyczka"); //post
+define("LAN_LOGINMENU_46", "ostatnie pozycje -");
+define("LAN_LOGINMENU_47", "Login menu - Najnowsze dodatki");
+define("LAN_LOGINMENU_48", "Konfiguracja Menu");
+define("LAN_LOGINMENU_49", "E-mail: ");
+define("LAN_LOGINMENU_50", "Login lub E-mail: ");
+define("LAN_LOGINMENU_51", "Zaloguj");
+
+?>
