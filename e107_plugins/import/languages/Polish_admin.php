5368ba3be8ba861eebcbbce2f7ca9de77113654d
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:41:13 +0200

    Add files via upload

--- e107_plugins/import/languages/Polish_admin.php
+++ e107_plugins/import/languages/Polish_admin.php
@@ -1,60 +1,85 @@
 <?php
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/02/20                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
 
-define("LAN_CONVERT_03", "Należy uruchomić z E107 pustej bazy danych, innych niż główny admin użytkownika (Identyfikator = 1)");
-define("LAN_CONVERT_04", "Puste pola, proszę wrócić i ponownie wprowadzić wartości.");
-define("LAN_CONVERT_05", "Uruchomienie tego skryptu może opróżnić wielu tabel E107 - upewnij się, że masz pełną kopię zapasową przed kontynuowaniem!");
-define("LAN_CONVERT_06", "Typ danych importu");
-define("LAN_CONVERT_07", "Specyfikacja formatu CSV");
-define("LAN_CONVERT_08", "Istniejącej bazy danych");
-define("LAN_CONVERT_09", "Szczegóły połączenia dla urządzenie źródłowe bazy danych");
-define("LAN_CONVERT_10", "Hasła w pliku źródłowym nie są szyfrowane.");
-define("LAN_CONVERT_11", "Szczegóły źródła danych");
-define("LAN_CONVERT_12", "Podstawowa nazwa użytkownika i hasło");
-define("LAN_CONVERT_13", "Pliku CSV");
-define("LAN_CONVERT_14", "Format importu bazy danych");
-define("LAN_CONVERT_15", "Nie dostępne konwertery importu");
-define("LAN_CONVERT_16", "Klas użytkownika początkowego");
-define("LAN_CONVERT_17", "Hasła w pliku CSV nie jest już zaszyfrowany");
-define("LAN_CONVERT_18", "(Hasło musi być przechowywane z szyfrowaniem MD5)");
-define("LAN_CONVERT_19", "Bazy danych hosta");
-define("LAN_CONVERT_20", "Nazwa użytkownika bazy danych");
-define("LAN_CONVERT_21", "Hasło bazy danych");
-define("LAN_CONVERT_22", "Nazwa bazy danych");
-define("LAN_CONVERT_23", "Prefiks tabeli bazy danych");
-define("LAN_CONVERT_24", "Obszary do zaimportowania");
-define("LAN_CONVERT_25", "Użytkownicy");
-define("LAN_CONVERT_26", "Forum definicje");
-define("LAN_CONVERT_27", "Ankiety");
-define("LAN_CONVERT_28", "Aktualności");
-define("LAN_CONVERT_29", "Import bazy danych zakończone");
-define("LAN_CONVERT_30", "Procedura importu informacji");
-define("LAN_CONVERT_31", "Plik danych CSV nie istnieje, lub nieprawidłowe uprawnienia");
-define("LAN_CONVERT_32", "Błąd podczas odczytywania pliku danych CSV");
-define("LAN_CONVERT_33", "Błąd w wierszu danych CSV");
-define("LAN_CONVERT_34", "Błąd:--ERRNUM--podczas zapisu do bazy danych użytkowników, linia");
-define("LAN_CONVERT_35", "CSV import zakończony. --LINIE--,--użytkowników--użytkownicy dodani,--błędy--błędy odczytu");
-define("LAN_CONVERT_36", "Nazwa pliku dla danych CSV");
-define("LAN_CONVERT_37", "Nieprawidłowy format specyfikacji dla typ importu");
-define("LAN_CONVERT_38", "Usuwanie istniejących danych");
-define("LAN_CONVERT_39", "(Jeśli nie, pojawi się plakaty importowanych danych jako 'Anonymous')");
-define("LAN_CONVERT_40", "Istniejące dane usunięte");
-define("LAN_CONVERT_41", "Pole dostępu wymagane bazy danych jest pusta");
-define("LAN_CONVERT_42", "Błąd w pliku definicji - wymagana Klasa nie istnieje");
-define("LAN_CONVERT_43", "Błąd podczas łączenia z źródłowa baza danych");
-define("LAN_CONVERT_44", "Błąd konfiguracji kwerendy dla");
-define("LAN_CONVERT_45", "Nie można odczytać pliku kodu importu");
-define("LAN_CONVERT_46", "Błąd:--ERRNUM--podczas zapisywania--DB--bazy danych, linia");
-define("LAN_CONVERT_47", "Wsadowy--bloku--import zakończone. --LINIE--czytaj, dodać--użytkowników--,--błędy--błędy");
-define("LAN_CONVERT_48", "Posty na forum");
-define("LAN_CONVERT_49", "Drupal");
-define("LAN_CONVERT_50", "Podstawowe polecenie import");
-define("LAN_CONVERT_51", "W wersji docelowej Drupal.");
-define("LAN_CONVERT_52", "Drupal w wersji");
-define("LAN_CONVERT_53", "Podstawowy adres URL Drupal witryny (np. http://mydrupalsite.com).");
-define("LAN_CONVERT_54", "Drupal podstawowy adres URL");
-define("LAN_CONVERT_55", "Podstawowej Ścieżka adresu URL (tj., katalog) Drupal instalacji (np. / drupal /).");
-define("LAN_CONVERT_56", "Drupal ścieżki bazowej");
+/**
+ *	e107 Import plugin
+ *
+ *	@package	e107_plugins
+ *	@subpackage	import
+ *	@version 	$Id$;
+*/
+
+if (!defined("e107_INIT")) { exit; }
+
+//define("LAN_CONTINUE","Continue");		Now global definition
+
+// define("LAN_CONVERT_01","Import into e107");
+// define("LAN_CONVERT_02","Import data from Wordpress, Joomla, Drupal, Blogspot, RSS and other formats.");
+define("LAN_CONVERT_03","Musisz rozpocząć z czystą bazą danych e107, jako główny administrator (ID=1)");
+define("LAN_CONVERT_04","Pole/pola zostały puste, proszę wrócić i ponownie wprowadzić wartości.");
+define("LAN_CONVERT_05","Uruchomienie tego skryptu może opróżnić tabele e107 - upewnij się, że masz pełną kopię zapasową przed kontynuowaniem!");
+define("LAN_CONVERT_06","Typ importowanych danych");
+define("LAN_CONVERT_07","Specyfikacja formatu CSV");
+define("LAN_CONVERT_08","Istniejące bazy danych");
+define("LAN_CONVERT_09","Szczegóły połączeń do bazy źródłowej");
+define("LAN_CONVERT_10","Hasła w pliku źródłowym nie są szyfrowane");
+define("LAN_CONVERT_11","Szczegółowe dane źródłowe");
+define("LAN_CONVERT_12","Podstawowa nazwa użytkownika i hasło");
+define("LAN_CONVERT_13","Plik CSV");
+define("LAN_CONVERT_14","Format importowanej bazy danych");
+define("LAN_CONVERT_15","Brak dostępnych konwenterów importu");
+define("LAN_CONVERT_16","Początkowa klasa użytkownika(ów)");
+define("LAN_CONVERT_17","Hasło w pliku CSV nie jest już zaszyfrowane");
+define("LAN_CONVERT_18","(Hasło musi być zapisane za pomocą szyfrowania MD5)");
+define("LAN_CONVERT_19","Host bazy danych");
+define("LAN_CONVERT_20","Użytkownik bazy danych");
+define("LAN_CONVERT_21","Hasło bazy danych");
+define("LAN_CONVERT_22","Nazwa bazy danych");
+define("LAN_CONVERT_23","Prefiks tabel bazy danych");
+define("LAN_CONVERT_24","Obszary do importowania");
+define("LAN_CONVERT_25","Użytkownicy");
+define("LAN_CONVERT_26","Definicje forum");
+define("LAN_CONVERT_27","Ankiety");
+define("LAN_CONVERT_28","Aktualności");
+define("LAN_CONVERT_29","Import bazy danych zakończony");
+define("LAN_CONVERT_30","Import rutynowych informacji");//Import routine Information
+define("LAN_CONVERT_31","Plik z danymi CSV nie istnieje, lub brak uprawnień do odczytu");
+define("LAN_CONVERT_32","Bład odczytu pliku CSV");
+define("LAN_CONVERT_33","Błąd w pliku CSV w lini ");
+define("LAN_CONVERT_34","Błąd: --ERRNUM-- podczas zapisu do bazy danych użytkownika, linia ");
+define("LAN_CONVERT_35","Zakończono import pliku CSV. --LINES-- odczytano, --USERS-- dodanych użytkowników, --ERRORS-- błędów");
+define("LAN_CONVERT_36","Nazwa pliku danych CSV");
+define("LAN_CONVERT_37","Nieprawidłowa specyfikacja formatu dla typu importu");
+define("LAN_CONVERT_38","Usuń istniejące dane");
+define("LAN_CONVERT_39","(Jeżeli tego nie zrobisz, autorzy wpisów z importowanych danych będą pokazywani jako 'anonimowy')");
+define("LAN_CONVERT_40","Istniejące dane usunięte");
+define("LAN_CONVERT_41","Wymagane pole dostęp do bazy danych jest pusty");
+define("LAN_CONVERT_42","Błąd w pliku definicji - wymagana klasa nie istnieje");
+define("LAN_CONVERT_43","Błąd podczas łączenia się ze źródłową bazą danych");
+define("LAN_CONVERT_44","Błąd konfiguracji zapytań dla ");
+define("LAN_CONVERT_45","Nie można odczytać pliku importu kodu");
+define("LAN_CONVERT_46","Błąd: --ERRNUM-- podczas zapisywania do --DB-- bazy danych, linia ");
+define("LAN_CONVERT_47","Wsadowe --BLOCK-- importowanie zakończone. --LINES-- odczytano, --USERS-- dodanych, --ERRORS-- błedów");
+define("LAN_CONVERT_48","Wpisy na forum");
+
+define("LAN_CONVERT_49"," Drupal");
+define("LAN_CONVERT_50","Podstawowy import");
+define("LAN_CONVERT_51","Wersja docelowego Drupala.");
+define("LAN_CONVERT_52","Wersja Drupala");
+define("LAN_CONVERT_53","Adres url bazowy Drupala (np., http://mojastronadrupal.com).");
+define("LAN_CONVERT_54","Adres url Drupala");
+define("LAN_CONVERT_55","Ścieżka url (tzn., katalog) instalacji Drupala (np., /drupal/).");
+define("LAN_CONVERT_56","Ścieżka bazowa Drupala");
+?>
--- e107_plugins/import/languages/Polish_global.php
+++ e107_plugins/import/languages/Polish_global.php
@@ -1,8 +1,18 @@
 <?php
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/02/20                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
 
-define("LAN_PLUGIN_IMPORT_NAME", "Zaimportować do e107");
-define("LAN_PLUGIN_IMPORT_DESCRIPTION", "Importowanie danych z Wordpress, Joomla, Drupal, Blogpost, RSS i innych formatów.");
+define("LAN_PLUGIN_IMPORT_NAME", "Importowanie do e107");
+define("LAN_PLUGIN_IMPORT_DESCRIPTION", "Importowanie danych z Wordpress, Joomla, Drupal, Blogpost, RSS oraz innych formatów.");
+?>
