dc5d73fef5a8bffe436fc046a6fea5573d8fb3c7
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:48:19 +0200

    Add files via upload

--- e107_plugins/online/languages/Polish.php
+++ e107_plugins/online/languages/Polish.php
@@ -1,29 +1,39 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("LAN_LASTSEEN_1", "Ostatni widać Menu");
-define("LAN_ONLINE_TRACKING_MESSAGE", "Online śledzenie użytkownika jest wyłączony, włącz go [tutaj]");
-define("LAN_ONLINE_1", "Goście:");
-define("LAN_ONLINE_2", "Członkowie:");
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/03/22                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
+define("LAN_LASTSEEN_1", "Menu Ostatnio Widziani");
+define("LAN_ONLINE_TRACKING_MESSAGE", "Śledzenie użytkowników online, jest wyłaczone. Możesz włączyć je [tutaj]");
+define("LAN_ONLINE_1", "Gości:");
+define("LAN_ONLINE_2", "Użytkowników:");
 define("LAN_ONLINE_3", "Na tej stronie:");
-define("LAN_ONLINE_4", "Online");
-define("LAN_ONLINE_5", "");
-define("LAN_ONLINE_6", "Najnowsze Członkowskie:");
-define("LAN_ONLINE_7", "Wyświetlanie");
+define("LAN_ONLINE_4", "online");
+define("LAN_ONLINE_5", " ");
+define("LAN_ONLINE_6", "Najnowszy użytkownik:");
+define("LAN_ONLINE_7", "przegląda");
 define("LAN_ONLINE_8", "Najwięcej online:");
-define("LAN_ONLINE_9", "na");
-define("LAN_ONLINE_10", "Online Menu");
-define("LAN_ONLINE_11", "Całkowita członków zarejestrowanych:");
-define("LAN_ONLINE_ADMIN_1", "lastseen menu");
-define("LAN_ONLINE_ADMIN_2", "Podpis lastseen menu");
-define("LAN_ONLINE_ADMIN_3", "Liczba rekordów do wyświetlania");
-define("LAN_ONLINE_ADMIN_4", "online menu");
-define("LAN_ONLINE_ADMIN_5", "Menu online podpis");
-define("LAN_ONLINE_ADMIN_6", "Pokaż listę członków online?");
-define("LAN_ONLINE_ADMIN_7", "Pokaż rozszerzoną listę członków Internecie?");
-define("LAN_ONLINE_ADMIN_8", "Wyświetla listę oddzielonych przecinkami członków.");
-define("LAN_ONLINE_ADMIN_9", "Wyświetla listę członków przeglądania strony.");
-define("LAN_ONLINE_ADMIN_10", "Wyświetlić Gości online.");
+define("LAN_ONLINE_9", "dnia");
+define("LAN_ONLINE_10", "Menu online");
+define("LAN_ONLINE_11", "Łącznie zarejestrowanych użytkowników.");
+define("LAN_ONLINE_ADMIN_1", "menu ostatnio widziani");
+define("LAN_ONLINE_ADMIN_2", "Tytuł menu");
+define("LAN_ONLINE_ADMIN_3", "Ilość wyświetlanych");
+define("LAN_ONLINE_ADMIN_4", "menu online");
+define("LAN_ONLINE_ADMIN_5", "Tytuł menu online");
+define("LAN_ONLINE_ADMIN_6", "Pokazywać listę użytkowników online?");
+define("LAN_ONLINE_ADMIN_7", "Pokazywać dodatkowe informacje o użytkownikach online?");
+define("LAN_ONLINE_ADMIN_8", "Wyświetla listę użytkowników odzieloną przecinkami.");
+define("LAN_ONLINE_ADMIN_9", "Wyświetla listę użytkowników aktualnie przeglądających stronę.");
+define("LAN_ONLINE_ADMIN_10", "Pokazuj gości online.");
+
+
+?>
