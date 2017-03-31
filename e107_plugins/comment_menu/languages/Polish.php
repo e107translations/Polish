80ec42ea1649a5355bc18ffdb397b1cdc6794068
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:10:26 +0200

    Add files via upload

--- e107_plugins/comment_menu/languages/Polish.php
+++ e107_plugins/comment_menu/languages/Polish.php
@@ -1,17 +1,28 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("CM_L1", "Nie ma jeszcze komentarzy.");
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/02/20                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
+define("CM_L1", "Nie ma jeszcze żadnych komentarzy.");
 define("CM_L2", "");
-define("CM_L3", "Podpis");
-define("CM_L4", "Liczba komentarzy do wyświetlenia?");
-define("CM_L5", "Liczba znaków do wyświetlania?");
-define("CM_L6", "Przyrostkowe na zbyt długie komentarze?");
-define("CM_L7", "Pokaż oryginalny tytuł wiadomości w menu?");
-define("CM_L8", "Nowe komentarze Menu konfiguracji");
-define("CM_L11", "na");
-define("CM_L12", "Re:");
-define("CM_L13", "Wysłany przez");
+define("CM_L3", "Nagłówek");
+define("CM_L4", "Ilość wyświetlanych komentarzy");
+define("CM_L5", "Ilość wyświetlanych znaków");
+define("CM_L6", "Przyrostek dla zbyt długich komentarzy");
+define("CM_L7", "Pokazywać oryginalny nagłówek wiadomości w menu");
+define("CM_L8", "Konfiguracja menu Ostatnie komentarze");
+//define("CM_L9", "Update Menu Settings");
+//define("CM_L10", "Comments menu configuration saved");
+define("CM_L11", "dnia");
+define("CM_L12", "Odp:");
+define("CM_L13", "Autor");
+
+?>
