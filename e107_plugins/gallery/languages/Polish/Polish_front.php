00d13e11d9878c5bc3ac2ae9e90a14398a816ce9
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:40:04 +0200

    Add files via upload

--- e107_plugins/gallery/languages/Polish/Polish_front.php
+++ e107_plugins/gallery/languages/Polish/Polish_front.php
@@ -1,7 +1,17 @@
 <?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Polish Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/03/28 18:50:30
+|
+|        $Author: Jacek Cichowski $
++---------------------------------------------------------------+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+define("LAN_GALLERY_FRONT_01", "Prawy przycisk > zapisz link jako");
+define("LAN_GALLERY_FRONT_02", "Rozwiń obraz");
 
-define("LAN_GALLERY_FRONT_01", "Kliknij prawym przyciskiem myszy > Zapisz element docelowy jako");
+
+?>
--- e107_plugins/gallery/languages/Polish/Polish_global.php
+++ e107_plugins/gallery/languages/Polish/Polish_global.php
@@ -1,12 +1,22 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/02/20                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 define("LAN_PLUGIN_GALLERY_TITLE", "Galeria");
-define("LAN_PLUGIN_GALLERY_DIZ", "Prosta Galeria");
-define("LAN_PLUGIN_GALLERY_SEF_01", "Galeria SEF");
-define("LAN_PLUGIN_GALLERY_SEF_02", "SEF URL włączone.");
-define("LAN_PLUGIN_GALLERY_SEF_03", "SEF URL wyłączone.");
-define("LAN_PLUGIN_GALLERY_SEF_04", "Domyślny Galeria");
+define("LAN_PLUGIN_GALLERY_DIZ", "Galeria zdjęć");
+define("LAN_PLUGIN_GALLERY_SEF_01", "Adresy SEF galerii");
+define("LAN_PLUGIN_GALLERY_SEF_02", "Włączono adresy SEF");
+define("LAN_PLUGIN_GALLERY_SEF_03", "Wyłączono adresy SEF");
+define("LAN_PLUGIN_GALLERY_SEF_04", "Domyślna galeria");
+
+
+?>
