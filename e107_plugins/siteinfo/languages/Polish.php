acc1d3e4fbf105bceacf1e24f18c0bbc78855b0d
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:51:02 +0200

    Add files via upload

--- e107_plugins/siteinfo/languages/Polish.php
+++ e107_plugins/siteinfo/languages/Polish.php
@@ -1,17 +1,28 @@
 <?php
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/03/22                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+define("COMPLIANCE_L1", "Zgodność W3C");
+define("SITEBUTTON_MENU_L1", "Link do strony");
+define("POWEREDBY_L1", "Zasilany przez ");
 
-define("COMPLIANCE_L1", "Zgodność z W3C");
-define("SITEBUTTON_MENU_L1", "Link do nas");
-define("POWEREDBY_L1", "Powered by");
-define("COUNTER_L1", "Admin wizyty nie są liczone.");
-define("COUNTER_L2", "Dziś Tę stronę...");
+define("COUNTER_L1", "Wizyty administratora nie są zliczane.");
+define("COUNTER_L2", "Ta strona dziś ...");
 define("COUNTER_L3", "łącznie");
-define("COUNTER_L4", "Ta strona kiedykolwiek...");
-define("COUNTER_L5", "Wyjątkowy");
-define("COUNTER_L6", "Miejscu...");
+define("COUNTER_L4", "Ta strona kiedykolwiek ...");
+define("COUNTER_L5", "unikalny");
+define("COUNTER_L6", "Strona ...");
 define("COUNTER_L7", "Licznik");
-define("COUNTER_L8", "Wiadomość admin: <b>Stat rejestrowanie jest wyłączone.</b><br>Aby aktywować, należy zainstalować wtyczkę statystyka logowania <a href=''.e_ADMIN.'plugin.php'>Menedżera dodatków</a>, a następnie aktywować go z <a href=''.e_PLUGIN.'log/admin_config.php'>ekranu konfiguracji</a>.");
+define("COUNTER_L8", "Wiadomość admina: <b>Statystyki są wyłączone.</b><br />Aby aktywować, musisz zainstalować plugin Statystyki, znajdujący się w <a href='".e_ADMIN."plugin.php'>menadżerze pluginów</a>, a następnie aktywować go w <a href='".e_PLUGIN."log/admin_config.php'>ustawieniach</a>.");
+
+?>
