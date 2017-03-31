d6a150ba18d841b95108397bdbd607bf3073cd17
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:45:43 +0200

    Add files via upload

--- e107_plugins/newforumposts_main/languages/Polish.php
+++ e107_plugins/newforumposts_main/languages/Polish.php
@@ -1,37 +1,49 @@
 <?php
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/03/22                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
 
-define("NFPM_LAN_1", "Wątek");
-define("NFPM_LAN_2", "Plakat");
-define("NFPM_LAN_3", "Widoki");
-define("NFPM_LAN_4", "Odpowiedzi");
-define("NFPM_LAN_5", "Lastpost");
-define("NFPM_LAN_6", "Wątki");
+define("NFPM_LAN_1", "Temat");
+define("NFPM_LAN_2", "Autor");
+define("NFPM_LAN_3", "Odsłon");
+define("NFPM_LAN_4", "Posty");
+define("NFPM_LAN_5", "Ostatni post");
+define("NFPM_LAN_6", "Tematów");
 define("NFPM_LAN_7", "przez");
-define("NFPM_L1", "Ten plugin wyświetla listę nowych postów na Twojej stronie");
-define("NFPM_L2", "Najnowsze posty na Forum");
-define("NFPM_L3", "Aby skonfigurować proszę kliknij na link w sekcji wtyczek admin strony");
-define("NFPM_L4", "Włączyć w obszar?");
-define("NFPM_L5", "Nieaktywne");
-define("NFPM_L6", "Początek strony");
-define("NFPM_L7", "U dołu strony");
-define("NFPM_L8", "Podpis");
-define("NFPM_L9", "Liczba nowych postów do wyświetlenia?");
-define("NFPM_L10", "Wyświetlania w programie przewijanie warstwy?");
-define("NFPM_L11", "Wysokość warstwy");
-define("NFPM_L12", "Nowe Forum posty konfiguracji");
-define("NFPM_L13", "Nowe posty Forum ustawienia aktualizacja.");
-define("NFPM_L14", "Wyświetlić najnowsze posty na forum?");
-define("NFPM_L15", "Domyślnie wyświetlane są najnowsze tematy.");
-define("NFPM_L16", "[użytkownik usunięty]");
-define("NFPM_L17", "Nowe posty na popularny wątek");
-define("NFPM_L18", "Nowe posty");
-define("NFPM_L19", "Brak nowych postów w wątku popularne");
-define("NFPM_L20", "Brak nowych postów");
-define("NFPM_L21", "Umocowany wątek");
-define("NFPM_L22", "Wątek zamknięty");
-define("NFPM_L23", "Ogłoszenie");
-define("NFPM_L24", "Wątek zamknięty");
+
+define("NFPM_L1", "Ten plugin wyświetla na stronie głównej listę nowych postów na forum.");
+define("NFPM_L2", "Nowe posty na forum");
+define("NFPM_L3", "Aby skonfigurować plugin kliknij na linku w sekcji pluginów na stronie Panel Administratora");
+define("NFPM_L4", "W której strefie aktywować ?");
+define("NFPM_L5", "Nieaktywny");
+define("NFPM_L6", "Na górze strony");
+define("NFPM_L7", "Na dole strony");
+define("NFPM_L8", "Nagłówek");
+define("NFPM_L9", "Ilość wyświetlanych postów ?");
+define("NFPM_L10", "Wyświetlić wewnątrz przewijaneej sekcji ?");
+define("NFPM_L11", "Wysokość sekcji");
+define("NFPM_L12", "Konfiguracja Nowe posty na forum");
+define("NFPM_L13", "Aktualizuj ustawienia");
+define("NFPM_L14", "Ustawienia pluginu zostały zaktualizowane.");
+define("NFPM_L15", "Aby wyświetlić najnowsze posty na forum zaznacz pole.<br />Domyślnie jest najnowszy wątek.");
+define('NFPM_L16', '[użytkownik został usunięty]');
+define('NFPM_L17', 'Nowe posty w popularnych wątkach');
+define('NFPM_L18', 'Nowe posty');
+define('NFPM_L19', 'Brak nowych postów w popularnych wątkach');
+define('NFPM_L20', 'Brak nowych postów');
+define('NFPM_L21', 'Przyklejony wątek');
+define('NFPM_L22', 'Zamknięty przyklejony wątek');
+define('NFPM_L23', 'Ogłoszenie');
+define('NFPM_L24', 'Zamknięty wątek');
+
+?>
