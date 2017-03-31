5090188bd05485f5272da07090d06d8775c7f801
Jacek <jacek_cich@poczta.fm>
2017-03-31 22:38:50 +0200

    Add files via upload

--- e107_plugins/featurebox/languages/Polish_admin_featurebox.php
+++ e107_plugins/featurebox/languages/Polish_admin_featurebox.php
@@ -1,13 +1,50 @@
 <?php
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/02/20                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
+TODO LAN - cleanup constants not in use
+Work in progress Moc - commented out are candidate for removal
+LAN file needs working from scratch after development is stabilized
+*/
 
+
+//define("LAN_PLUGIN_FEATUREBOX_NAME", "Feature Box");
+//define("FBLAN_02", "Displays an animated area on the top of your page with news-items and other content you would like to feature.");
+//define("FBLAN_03", "Configure feature box"); //FIXME LAN  - use generic.
+//define("FBLAN_04", "Feature Box plugin has been successfully installed. To add messages and configure, return to the main admin page and click on the feature box icon in the plugin section.");
+//define("FBLAN_05", "No feature box messages defined yet");
+//define("FBLAN_06", "Existing Feature Box Messages"); //FIXME LAN  - use generic.
+//define("FBLAN_07", "Title / Caption"); //FIXME LAN  - use generic.
 define("FBLAN_08", "Tekst wiadomości");
-define("FBLAN_12", "Tryb");
-define("FBLAN_13", "Losowo krążą wiadomości");
-define("FBLAN_14", "Pokaż tylko ten komunikat");
-define("FBLAN_22", "Typ renderowania");
-define("FBLAN_23", "W polu tematu");
-define("FBLAN_24", "Zwykły");
+//define("FBLAN_09", "Visibility of message");
+//define("FBLAN_10", "Create Feature Box Message"); //FIXME LAN  - use generic.
+//define("FBLAN_11", "Update Feature Box Message"); //FIXME LAN  - use generic.
+define("FBLAN_12", "Mod");
+define("FBLAN_13", "Wiadomość pokazywana losowo");
+define("FBLAN_14", "Pokazuj tylko tę wiadomość");
+//define("FBLAN_15", "Message added to database."); //FIXME LAN  - use generic.
+//define("FBLAN_16", "Message updated in database."); //FIXME LAN  - use generic.
+//define("FBLAN_17", "Field(s) left blank");
+//define("FBLAN_18", "Feature Box Message deleted"); //FIXME LAN  - use generic.
+//define("FBLAN_19", "Options"); //FIXME LAN  - use generic.
+//define("FBLAN_20", "Edit"); //FIXME LAN  - use generic.
+//define("FBLAN_21", "Delete"); //FIXME LAN  - use generic.
+define("FBLAN_22", "Sposób wyświetlania");
+define("FBLAN_23", "Zgodnie ze stylem używanego tematu");
+define("FBLAN_24", "Prosty");
+//define("FBLAN_25", "Template"); //FIXME LAN  - use generic.
+//define("FBLAN_26", "you can use a different template for each message, add templates to e107_plugins/featurebox/templates/ folder");
+
+
+?>
--- e107_plugins/featurebox/languages/Polish_global.php
+++ e107_plugins/featurebox/languages/Polish_global.php
@@ -1,14 +1,25 @@
 <?php
+/*
++====================================================================+
++--------------------------------------------------------------------+
+   * Spolszczenie systemu CMS e107                                   *
+   * Polish language file                                            *
+	* e107 Polish Team                                                *
+	* www.d4e.pl                                                      *
+	* $Date: 2016/02/20                                               *
+	* $Author: JacekCich                                              *
++--------------------------------------------------------------------+
++====================================================================+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+define("LAN_PLUGIN_FEATUREBOX_NAME", 		"Wiadomości specjalne");
+define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Plugin Wiadomości specjalne (Feature Box) umożliwia wyświetlanie, ponad pozycjami aktualności, komunikatów, które uznasz za ważne, lub czegokolwiek innego co będziesz chciał umieścić w tym miejscu. Wiadomości mogą zmieniać się losowo, albo zanikac dynamicznie.");
+define("LAN_PLUGIN_FEATUREBOX_BATCH",  		"Utwórz wiadomość");
 
-define("LAN_PLUGIN_FEATUREBOX_NAME", "Feature Box");
-define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Wyświetla obszar animowany, na górze strony-newsy i inne treści, które chcieliby Państwo dysponują.");
-define("LAN_PLUGIN_FEATUREBOX_BATCH", "Tworzenie elementu Featurebox");
-define("FBLAN_INSTALL_01", "Dodawanie domyślnej kategorii tabeli danych.");
-define("FBLAN_INSTALL_02", "Dodawanie domyślnej tabeli danych.");
+
+// Install/uninstall messages
+define("FBLAN_INSTALL_01", "Dodawanie domyślnych danych do tabeli bazy danych."); //XXX Use Generic
+define("FBLAN_INSTALL_02", "Dodawanie domyślnych danych."); //XXX Use Generic
 define("FBLAN_INSTALL_03", "Nieprzypisane");
 define("FBLAN_INSTALL_04", "Karuzela");
-define("FBLAN_INSTALL_05", "Zakładki");
+define("FBLAN_INSTALL_05", "Tabela");
